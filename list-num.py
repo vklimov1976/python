bbe929fe86ac8876642ba0a01a94de243563db26
Sargon <vklimov1976@gmail.com>
2020-05-16 22:54:41 +0300

    just add new files

---
+++ list-num.py
@@ -0,0 +1,11 @@
+num = [0.8, 7.0, 6,8, -6]
+#for i in num:
+#    print(i, '-number')
+for i in num:
+    if i == 7.0:
+        print(i)
+###
+country = "Russia"
+for ch in country:
+    if ch.isupper():
+        print(ch)
