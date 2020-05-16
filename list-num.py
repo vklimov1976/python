6696c2da0aa650f03e2bf8d875f839249ea20c0c
Sargon <vklimov1976@gmail.com>
2020-05-17 00:29:25 +0300

    add task 10.1

--- list-num.py
+++ list-num.py
@@ -1,11 +1,20 @@
-num = [0.8, 7.0, 6,8, -6]
+#num = [0.8, 7.0, 6,8, -6]
 #for i in num:
 #    print(i, '-number')
-for i in num:
-    if i == 7.0:
-        print(i)
+#for i in num:
+#    if i == 7.0:
+#        print(i)
 ###
-country = "Russia"
-for ch in country:
-    if ch.isupper():
-        print(ch)
+#country = "Russia"
+#for ch in country:
+#    if ch.isupper():
+#        print(ch)
+###
+'''
+total = 0
+for i in range(1, 101):
+    total = total + i # total += i
+    print(total)
+# Все эти строки, заменяются одной стракой ниже
+'''
+print (sum(list(range(1, 101))))
