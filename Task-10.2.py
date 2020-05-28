c7013a2c7b1e4c95f1e996e63b5843020b7b70b3
Klimov Vasily <vklimov1976@gmail.com>
2020-05-28 21:47:36 +0300

    Add Task-10.2

--- Task-10.2.py
+++ Task-10.2.py
@@ -1,14 +1,21 @@
 '''
 Упражнение 10.2
-L = [-8, 8, 6.0, 5, 'строка', -3.1]
+L = [-8, 8, 6.0, 5, 'string', -3.1]
 Определить сумму чисел, входящих в список L. Подсказка: для определения типа объекта
 можно воспользоваться сравнением вида type(-8) == int.
 '''
 L = [-8, 8, 6.0, 5, 'string', -3.1]
+
+# first method
+
+#for i in L:
+#    if type(i) == str:
+#        x = L.remove(i)
+#print(sum(L))
+
+# Second method
+x = 0
 for i in L:
-    if type(!str):
-        print(sum(i))
-
-
-
-    
+    if type(i) == int or type(i) == float:
+        x += i
+print(x)
